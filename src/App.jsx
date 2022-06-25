2c5041cdd0f1d56226cd5e94d0e902562e9b3091
Deepudeepthi555 <deepikamohandeepz99@gmail.com>
2022-06-25 14:33:26 +0530

    Updated: app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 import HeroPage from "./pages/heropage";
+import SignupPage from "./pages/signup_driver";
+import AvailDriver from "./pages/availdriver";
 
 function App() {
   return (
     <>
       <HeroPage />
+      <SignupPage />
+      <AvailDriver />
     </>
   );
 }
